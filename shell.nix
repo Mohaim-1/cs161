a19393ee8308904de6fc2a3959d548108eb8b526
Mohaim-1 <61601784+Mohaim-1@users.noreply.github.com>
2025-01-07 10:23:56 -0800

    update w1p2

--- shell.nix
+++ shell.nix
@@ -1,14 +1,10 @@
 # shell.nix
 let
-  # We pin to a specific nixpkgs commit for reproducibility.
-  # Last updated: 2024-04-29. Check for new commits at https://status.nixos.org.
   pkgs = import <nixpkgs> {};
 in pkgs.mkShell {
   packages = [
     (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
       # select Python packages here
-      # pandas
-      # requests
     ]))
   ];
 }
--- week01/problem02.py
+++ week01/problem02.py
@@ -1,16 +1,17 @@
 # Katherine Fournier
 # CS 161, Week 1, Problem 1
 
-print("Enter your first name: ", end="")
-name = input()
+def main():
+    print("Enter your first name: ", end="")
+    name = input()
 
-age = get_int("Enter your current age: ")
-yearly_savings = prompt_int("Enter your yearly savings: ")
+    age = prompt_int("Enter your current age: ")
+    yearly_savings = prompt_int("Enter your yearly savings: ")
 
-print((f"Hello {name}! You are currently {age} years old.\n"
-       f"In 10 years, you will be {age+10} years old.\n"
-       f"If you save ${yearly_savings} each year, in 5 years you will have saved ${yearly_savings*5}.\n"
-       f"Your average monthly savings is ${yearly_savings/12:.2f}."))
+    print((f"Hello {name}! You are currently {age} years old.\n"
+           f"In 10 years, you will be {age+10} years old.\n"
+           f"If you save ${yearly_savings} each year, in 5 years you will have saved ${yearly_savings*5}.\n"
+           f"Your average monthly savings is ${yearly_savings/12:.2f}."))
 
 def prompt_int(prompt) -> int:
     number = None
@@ -21,3 +22,5 @@ def prompt_int(prompt) -> int:
         except ValueError:
             print("Please enter a whole number.")
     return number
+
+main()
