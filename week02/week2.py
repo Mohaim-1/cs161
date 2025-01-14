0d483d1da286da082a932d24b7c03ab5bbff644f
Mohaim-1 <61601784+Mohaim-1@users.noreply.github.com>
2025-01-13 17:48:39 -0800

    change inputs to set values

--- week02/week2.py
+++ week02/week2.py
@@ -1,12 +1,19 @@
 # Katie Fournier
 # CS 161, Week 2
 
+import sys
+
+
 def part_1():
-    # Problem 1 and 2
-    x = 18
+
+    # Problems 1 and 2
     # bin() and hex() take an integer.
     # If x is the wrong data type (such as a float), we get a TypeError.
-    print(x, bin(x), hex(x))
+    try:
+        x = 18
+        print(x, bin(x), hex(x))
+    except TypeError:
+        sys.exit("x must be an integer.")
 
     # Problem 3
     y = 0b1011
@@ -15,42 +22,28 @@ def part_1():
 
     # Problem 4
     w = x + y + z
-    print('the sum is ', w)
+    print('the sum is', w)
 
 
 def compression():
     """Calculates a compression percentage based on set values."""
 
-    original_size = input_pos_int("Original data size: ")
-    dictionary_size = input_pos_int("Dictionary size: ")
-    compressed_text_size = input_pos_int("Compressed text size: ")
+    original_size = 240
+    dictionary_size = 25
+    compressed_text_size = 148
 
     total_compressed_size = dictionary_size + compressed_text_size
+    if original_size < 1 or total_compressed_size < 1:
+        sys.exit("original size and compressed size must be positive.")
     compression_amount = f"{1 - (total_compressed_size / original_size):.2%}"
 
-    print()
-    print((f"Compressed text size: {compressed_text_size} characters\n"
+    print((f"\n"
+           f"Compressed text size: {compressed_text_size} characters\n"
            f"     Dictionary size: {dictionary_size} characters\n"
-           f"               Total: {compressed_text_size + dictionary_size} characters\n"
+           f"               Total: {total_compressed_size} characters\n"
            f"  Original text size: {original_size} characters\n"
            f"         Compression: {compression_amount}"))
 
 
-def input_pos_int(prompt) -> int:
-    """Repeats a prompt until the user enters a positive integer."""
-
-    pos_int = 0
-    while pos_int < 1:
-        print(prompt, end='')
-        try:
-            pos_int = int(input())
-            if pos_int < 1:
-                raise ValueError
-        except ValueError:
-            print("Please enter a positive whole number.")
-    return pos_int
-
-
-# part_1()
+part_1()
 compression()
-
